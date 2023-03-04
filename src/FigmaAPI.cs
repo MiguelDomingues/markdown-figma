2cb5bf6f86f2a36baca6385f4afc0a2b7d7a648d
Miguel Domingues <miguelbrazaodomingues@gmail.com>
2023-03-04 13:01:34 +0000

    Parallelize retrieval of export urls

--- src/FigmaAPI.cs
+++ src/FigmaAPI.cs
@@ -78,11 +78,19 @@ namespace MarkdownFigma
             return node;
         }
 
-        private static Dictionary<string, string> GetExportUrls(string token, string key, string ids, FigmaFormat format)
+        private static Dictionary<string, string> GetExportUrls(string token, string key, IEnumerable<string> ids, FigmaFormat format)
         {
-            FigmaImagesExport f = Get<FigmaImagesExport>(token, $"images/{key}?ids={ids}&format={format.ToString().ToLower()}", null);
+            ConcurrentDictionary<string, string> result = new ConcurrentDictionary<string, string>();
+            ids.AsParallel().ForAll(id =>
+            {
+                FigmaImagesExport f = Get<FigmaImagesExport>(token, $"images/{key}?ids={id}&format={format.ToString().ToLower()}", null);
+                foreach (KeyValuePair<string, string> i in f.Images)
+                {
+                    result.AddOrUpdate(i.Key, i.Value, (key, oldValue) => i.Value);
+                }
+            });
 
-            return f.Images;
+            return result.ToDictionary(entry => entry.Key, entry => entry.Value);
         }
 
         internal static IEnumerable<UpdateReport> ExportNodesTo(string figmaToken, string figmaURL, string exportPath, bool ignoreDuplicates, bool svgVisualCheckOnly, IEnumerable<string> includeOnly, double threshold)
@@ -120,8 +128,7 @@ namespace MarkdownFigma
                 if (formatChilds.Count() == 0)
                     continue;
                 Log.Information("Obtaining download urls for {Count} {Format} elements...", formatChilds.Count(), format);
-                string ids = string.Join(",", formatChilds.Select(c => c.Id));
-                Dictionary<string, string> downloadUrls = GetExportUrls(figmaToken, fileKey, ids, format);
+                Dictionary<string, string> downloadUrls = GetExportUrls(figmaToken, fileKey, formatChilds.Select(c => c.Id), format);
                 downloadUrls.AsParallel().ForAll(dl =>
                 {
                     string name = formatChilds.Where(c => c.Id == dl.Key).First().Name.Trim();
